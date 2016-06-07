1245c0f5f38bf8fa444993c673498b044f6aafb9
vjcspy <mr.vjcspy@gmail.com>
2016-06-07 18:28:40 +0700

    - lam chuc nang support config qua xml

---
+++ Repositories/Theme/View/IzAdmin/Index.php
@@ -0,0 +1,23 @@
+<?php
+namespace Modules\IzCustomer\Repositories\Theme\View\IzAdmin;
+
+use Modules\IzCore\Repositories\Theme\View\AdditionViewInterface;
+
+/**
+ * Created by PhpStorm.
+ * User: vjcspy
+ * Date: 07/06/2016
+ * Time: 17:10
+ */
+class Index implements AdditionViewInterface {
+
+    /**
+     * @return []
+     */
+    public function handle() {
+        // TODO: Implement handle() method.
+        return [
+            'custome_data' => 'nothing'
+        ];
+    }
+}
